26c1d0dbb96336df848649e2d9dc42c197d0c247
Chrissus14 <cjgm14@gmail.com>
2020-07-08 21:01:13 -0500

    modify data

--- src/data.js
+++ src/data.js
@@ -6,30 +6,6 @@ export default [
     description:
       "Lista de tareas en la que puedes crear, completar, eliminar una o varias tareas",
     link: "https://myowntodolist.netlify.app/",
-    tools: ["React", "Styled components", "Local storage"],
-  },
-  {
-    id: 3,
-    image: "/images/landing-page.PNG",
-    title: "Fylo landing page",
-    description: "Landing page challenge de frontendmentor.io",
-    link: "https://fylo-landing-page-two-roan.now.sh/",
-    tools: ["HTML", "CSS"],
-  },
-  {
-    id: 4,
-    image: "/images/single-section.PNG",
-    title: "Sección de introducción",
-    description: "Single introductory challenge de frontendmentor.io",
-    link: "https://single-introductory-section.now.sh/",
-    tools: ["HTML", "CSS"],
-  },
-  {
-    id: 5,
-    image: "/images/hudle-landing.PNG",
-    title: "Hudle Landing Page",
-    description: "Landing page challenge de frontendmentor.io",
-    link: "https://huddle-landing-page-alternating.now.sh/",
-    tools: ["HTML", "CSS"],
+    tools: ["React", "Styled components"],
   },
 ];
